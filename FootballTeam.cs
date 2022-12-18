a9da1ebc11fbfc951c212c40f2769dd6c9063fad
Kieran Emery <kizwiz@hotmail.co.uk>
2022-12-18 21:41:48 +0000

    Add project files.

---
+++ FootballTeam.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PremierLeague
+{
+    abstract class FootballTeam
+    {
+        public string Name { get; set; }
+        public string Stadium { get; set; }
+
+        public FootballTeam(string name, string stadium)
+        {
+            Name = name;
+            Stadium = stadium;
+        }
+    }
+}
