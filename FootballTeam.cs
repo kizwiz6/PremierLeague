326c664853c0621958a1810263ba32a1b49974cc
Kieran Emery <kizwiz@hotmail.co.uk>
2022-12-18 23:47:38 +0000

    End of day commit

--- FootballTeam.cs
+++ FootballTeam.cs
@@ -6,15 +6,36 @@ using System.Threading.Tasks;
 
 namespace PremierLeague
 {
+    /// <summary>
+    /// Single Responsibility Principle: This class has only one responsibility, which is to represent a Premier League football team.
+    /// </summary>
     abstract class FootballTeam
     {
         public string Name { get; set; }
         public string Stadium { get; set; }
+        public string Manager { get; set; }
+        public int Wins { get; set; }
+        public int Losses { get; set; }
+        public int Draws { get; set; }
 
         public FootballTeam(string name, string stadium)
         {
             Name = name;
             Stadium = stadium;
+        }
+
+        //public FootballTeam(string name, int wins, int losses, int draws)
+        //{
+        //    Name = name;
+        //    Wins = wins;
+        //    Losses = losses;
+        //    Draws = draws;
+        //}
+
+        // Calculate the team's points based on their wins, losses, and draws
+        public int CalculatePoints()
+        {
+            return (Wins * 3) + Draws;
         }
     }
 }
